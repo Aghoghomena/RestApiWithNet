951eedb9b3473ef63682ae1eeb7a1e1519bf68d0
Aghoghomena <aghoghojao@gmail.com>
2021-05-26 18:35:20 +0100

    Integrate Swagger

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -33,11 +33,25 @@ namespace WebApplication1
             //add employeedto
             //services.AddSingleton<IEmployeeData, EmployeeImplementation>();
             services.AddScoped<IEmployeeData, sqlEmployee>();
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                //set it to startup route or path
+                c.RoutePrefix = "";
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
